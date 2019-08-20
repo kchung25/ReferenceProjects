6ca7b407d0ddaebec83959000cb9190a4e00117c
Kenway Chung <chung_kenway@bah.com>
2019-08-20 19:41:27 -0400

    Apply middleware and add action creators

--- redux-guide/src/containers/Counter/Counter.js
+++ redux-guide/src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import {increment, decrement, add, subtract, storeResult, deleteResult} from '../../store/actions/actions';
 class Counter extends Component {
     state = {
         counter: 0
@@ -55,12 +55,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-        onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 5 }),
-        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 5 }),
-        onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
-        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id}),
+        onIncrementCounter: () => dispatch(increment()),
+        onDecrementCounter: () => dispatch(decrement()),
+        onAddCounter: () => dispatch(add(5)),
+        onSubtractCounter: () => dispatch(subtract(5)),
+        onStoreResult: (result) => dispatch(storeResult(result)),
+        onDeleteResult: (id) => dispatch(deleteResult(id)),
     };
 }
 
--- redux-guide/src/index.js
+++ redux-guide/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -13,7 +13,19 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-const store = createStore(rootReducer);
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching: ', action);
+            const result = next(action);
+            console.log('[Middleware] Dispatching: ', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
